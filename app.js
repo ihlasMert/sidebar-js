f18fa977a82cd68dfe414306430ef056feb3e75b
İHLASMERT <ihlas1mert@gmail.com>
2023-01-08 20:55:47 +0300

    added if statements

--- app.js
+++ app.js
@@ -4,5 +4,9 @@ const sidebar = document.querySelector(".sidebar");
 
 
 toggleBtn.addEventListener("click",function(){
-    
+if(sidebar.classList.contains("show-sidebar")){
+    sidebar.classList.remove("show-sidebar")
+}else{
+    sidebar.classList.add("show-sidebar")
+}
 })
